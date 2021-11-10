57ccf57b05cf31b9e26725223291d7c24c79aa84
Dayane <siminskidayane@gmail.com>
2021-11-10 00:11:12 -0300

    ultimo commit

--- Lista/src/Main/Main.java
+++ Lista/src/Main/Main.java
@@ -1,25 +1,52 @@
 package Main;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+            List<String> num = new ArrayList<>();
+            float media = 0;
+            int maior=0;
+            int menor=0;
 
-        Scanner leitor = new Scanner(System.in);
-        int i,
 
-        for(int i= 0; i <10; i++){
-            System.out.println("Digite um valor:");
-            int num = leitor.nextInt();
-            valor[i] =num;
+            num.add("50");
+            num.add("20");
+            num.add("30");
+            num.add("20");
+            num.add("120");
+            num.add("60");
+            num.add("70");
+
+
+            for (int i=0; i<num.size(); i++) {
+                Integer.parseInt(num.get(i));
+                int numero = Integer.parseInt(num.get(i));
+                media += numero;
+            }
+             media = media/7;
+             System.out.println(media);
+
+        for (int i = 0;i < num.size();i++){
+
+            if(i == 0){
+                maior = Integer.valueOf(num.get(i));
+                menor = Integer.valueOf(num.get(i));
+            }
+            if(maior < Integer.valueOf(num.get(i))){
+                maior = Integer.valueOf(num.get(i));
+            }
+            if(menor > Integer.valueOf(num.get(i))){
+                menor = Integer.valueOf(num.get(i));
+            }
         }
-        Arrays.sort(valor);
-        int maior =valor[9];
-        int menor =valor[0];
 
-        System.out.println("Maior ="+maior+"\nMenor="+menor);
-   }
-}
+
+        System.out.println("maior"+maior+"menor"+menor);
+        }
+
+    }
+
